0ba5a50d14fbf1cf026aa7773512e41aab93fc4e
BorjaManterola <bjmanterola@miuandes.cl>
2024-03-25 20:11:13 -0300

    Lab3

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+function submitButton() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+}
